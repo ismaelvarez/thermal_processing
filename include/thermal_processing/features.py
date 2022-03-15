70083982fb00c0124d33fd9c5fe0bd2688dafff9
Ismael <ismaelvarez96@gmail.com>
2022-03-15 08:34:24 +0000

    Added mean temperature by areas

--- include/thermal_processing/features.py
+++ include/thermal_processing/features.py
@@ -4,7 +4,7 @@ import numpy as np
 from cv_bridge import CvBridge
 
 
-def get_min_max_point(image, threshold=30000):
+def get_min_max_point(image):
     bridge = CvBridge()
     cv_image = bridge.imgmsg_to_cv2(image, desired_encoding='mono16')
     cv_image = cv2.GaussianBlur(cv_image, (41, 41), 0)
@@ -12,22 +12,62 @@ def get_min_max_point(image, threshold=30000):
     return maxLoc, maxVal, minLoc, minVal
 
 
-def get_max_temp_area(image):
+def calculate_mean_of_contours(image, mask):
+    contours = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[0]
+    means = []
+    positions = []
+    for contour in contours:
+        _, radius = cv2.minEnclosingCircle(contour)
+        if radius > 10:
+            mask_ = np.zeros(image.shape, dtype=np.uint8)
+            cv2.fillPoly(mask_, pts=[contour], color=(255, 255, 255))
+            means.append(cv2.mean(image, mask_)[0])
+            M = cv2.moments(contour)
+            positions.append([int(M['m10'] / M['m00']), int(M['m01'] / M['m00'])])
+    return means, positions
+
+
+def get_max_value(image, max_threshold):
+    _, mask = cv2.threshold(image, max_threshold, 255, cv2.THRESH_BINARY)
+    means, positions = calculate_mean_of_contours(image, mask)
+
+    mean_max = -1000
+    position = [0, 0]
+    for mean in means:
+        if mean > mean_max:
+            mean_max = mean
+            position = positions[means.index(mean)]
+
+    return mean_max, position
+
+
+def get_min_value(image, min_threshold):
+    _, mask = cv2.threshold(image, min_threshold, 255, cv2.THRESH_BINARY)
+    means, positions = calculate_mean_of_contours(image, cv2.bitwise_not(mask))
+    mean_max = 1000
+    position = [0, 0]
+    for mean in means:
+        if mean < mean_max:
+            mean_max = mean
+            position = positions[means.index(mean)]
+
+    return mean_max, position
+
+
+def get_max_min_values(image, max_threshold, min_threshold):
+    blur = cv2.GaussianBlur(image, (41, 41), 0)
+
+    max_value, position_max = get_max_value(blur, max_threshold)
+
+    min_value, position_min = get_min_value(blur, min_threshold)
+
+    return position_max, max_value, position_min, min_value
+
+
+def calculate_temperature_by_areas(image, max_threshold=200, min_threshold=40):
     bridge = CvBridge()
     cv_image = bridge.imgmsg_to_cv2(image, desired_encoding='mono8')
-
-    blur = cv2.GaussianBlur(cv_image, (5, 5), 0)
-    _, mask = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-
-    return mask
-
-
-def get_min_temp_area(image):
-    bridge = CvBridge()
-    cv_image = bridge.imgmsg_to_cv2(image, desired_encoding='mono16')
-
-    mask = cv2.inRange(cv_image, 0, 10000)
-    return np.column_stack(np.where(mask == 0))
+    return get_max_min_values(cv_image, max_threshold, min_threshold)
 
 
 def get_temperature(value, max_temp, min_temp, max_image_value):
--- include/thermal_processing/node_manager.py
+++ include/thermal_processing/node_manager.py
@@ -10,9 +10,14 @@ MONO16_MAX_VALUE = 65535
 IMAGE_PUBLISHER = None
 TEMPERATURE_PUBLISHER = None
 
+TEMPERATURE_CALCULATION_MODE = "area"
+
 
 def callback(data):
-    position_max, max_value, position_min, min_value = features.get_min_max_point(data, rospy.get_param("~threshold"))
+    scale = rospy.wait_for_message('thermal_image_temperature_scale', TemperatureScale)
+    position_max, max_value, position_min, min_value = \
+        features.calculate_temperature_by_areas(data, rospy.get_param('~max_threshold'),
+                                                rospy.get_param('~min_threshold'))
 
     if IMAGE_PUBLISHER:
         IMAGE_PUBLISHER.publish(utils.draw_min_max_temp(data, position_max, position_min))
@@ -21,7 +26,6 @@ def callback(data):
     if TEMPERATURE_PUBLISHER:
         # rospy.loginfo(rospy.get_caller_id() + 'maxLoc: %s; temp: %s ', position_max,
         #               features.get_temperature(max_value, TEMPERATURE.max, TEMPERATURE.min, MONO16_MAX_VALUE), )
-        scale = rospy.wait_for_message('thermal_image_temperature_scale', TemperatureScale)
         ir_info = IRCameraTemperatureInfo()
         ir_info.temperatureMax = features.get_temperature(max_value, scale.max, scale.min, MONO16_MAX_VALUE)
         ir_info.temperatureMin = features.get_temperature(min_value, scale.max, scale.min, MONO16_MAX_VALUE)
@@ -42,13 +46,15 @@ def init_subscribers():
 
 
 def init():
-
+    global TEMPERATURE_CALCULATION_MODE
     # In ROS, nodes are uniquely named. If two nodes with the same
     # name are launched, the previous one is kicked off. The
     # anonymous=True flag means that rospy will choose a unique
     # name for our 'listener' node so that multiple listeners can
     # run simultaneously.
     rospy.init_node('thermal-processing', anonymous=True)
+
+    TEMPERATURE_CALCULATION_MODE = rospy.get_param('~mode')
 
     init_publishers()
     init_subscribers()
